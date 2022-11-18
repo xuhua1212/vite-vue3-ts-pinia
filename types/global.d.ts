9b09efa8231bf1454f09a4637f6f58b85fa57732
xuhua <997559495@qq.com>
2022-11-18 17:52:26 +0800

    style(deps): 格式,引入环境变量

---
+++ types/global.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: xuhua
+ * @Date: 2022-11-18 17:12:00
+ * @LastEditors: xuhua
+ * @LastEditTime: 2022-11-18 17:12:02
+ * @FilePath: /vite-vue3-ts-pinia/global.d.ts
+ * @Description: 全局声明文件
+ */
+declare module 'crypto-js'
--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,70 @@
-import { defineConfig } from 'vite'
+/*
+ * @Author: xuhua
+ * @Date: 2022-11-18 15:54:25
+ * @LastEditors: xuhua
+ * @LastEditTime: 2022-11-18 17:47:25
+ * @FilePath: /vite-vue3-ts-pinia/vite.config.ts
+ * @Description:
+ */
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-const resolve = (dir) => path.resolve(__dirname, dir)
+import { createHtmlPlugin } from 'vite-plugin-html'
+
+const resolve = (dir: string) => path.resolve(__dirname, dir)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	resolve: {
-		alias: {
-			'@': resolve('src'),
+export default defineConfig(({ command, mode }) => {
+	console.log('defineConfig ~  command, mode', command, mode)
+	const env = loadEnv(mode, process.cwd())
+	console.log('defineConfig ~ env', env)
+	return {
+		resolve: {
+			alias: {
+				'@': resolve('src'),
+			},
 		},
-	},
-	plugins: [
-		AutoImport({
-			imports: ['vue', 'vue-router', '@vueuse/core', 'pinia'],
-			dts: 'src/auto-import.d.ts',
-			resolvers: [ElementPlusResolver()],
-		}),
-		Components({
-			dirs: ['src/components/'],
-			dts: 'src/components.d.ts',
-			resolvers: [ElementPlusResolver()],
-		}),
-		vue(),
-	],
-	server: {
-		port: 8080, //本地启动端口
-		open: true,
-		host: '0.0.0.0',
-		// hmr: {
-		//   host: "127.0.0.1",
-		//   port: 8080,
-		// },
-		// // 设置 https 代理
-		// proxy: {
-		//   "/api": {
-		//     target: "",
-		//     changeOrigin: true,
-		//     rewrite: (path: string) => path.replace(/^\/api/, ""),
-		//   },
-		// },
-	},
+		plugins: [
+			vue(),
+			AutoImport({
+				imports: ['vue', 'vue-router', '@vueuse/core', 'pinia'],
+				dts: 'src/auto-import.d.ts',
+				resolvers: [ElementPlusResolver()],
+			}),
+			Components({
+				dirs: ['src/components/'],
+				dts: 'src/components.d.ts',
+				resolvers: [ElementPlusResolver()],
+			}),
+			createHtmlPlugin({
+				minify: true,
+				inject: {
+					data: {
+						title: env.VITE_TITLE,
+					},
+				},
+			}),
+		],
+		server: {
+			port: 8080, //本地启动端口
+			open: true,
+			host: '0.0.0.0',
+			// hmr: {
+			//   host: "127.0.0.1",
+			//   port: 8080,
+			// },
+			// // 设置 https 代理
+			// proxy: {
+			//   "/api": {
+			//     target: "",
+			//     changeOrigin: true,
+			//     rewrite: (path: string) => path.replace(/^\/api/, ""),
+			//   },
+			// },
+		},
+	}
 })
